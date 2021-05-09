ead09ee0cf2cd2eed0a15b878c47422698c351b0
Siarhei Saukin <saukin@yahoo.com>
2021-05-09 10:15:10 -0400

    Initial Commit

---
+++ NoteCalculator/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  NoteCalculator
+//
+//  Created by Siarhei Saukin (Étudiant) on 2021-05-09.
+//  Copyright © 2021 Siarhei Saukin (Étudiant). All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
